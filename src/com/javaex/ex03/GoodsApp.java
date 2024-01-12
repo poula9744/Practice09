fb8a0321f457afe29fd66019b3c07fd72b868b6c
poula9744 <poula9744@naver.com>
2024-01-12 22:14:19 +0900

    수정

--- src/com/javaex/ex03/GoodsApp.java
+++ src/com/javaex/ex03/GoodsApp.java
@@ -11,20 +11,44 @@ public class GoodsApp {
 		ArrayList<Goods> gList = new ArrayList<Goods>();
 
 		String p;
+		int sum = 0;
+		int price;
+		int count = 0;
 
 		// 상품 입력
 		System.out.println("상품을 입력해주세요(종료 q)");
 		Scanner sc = new Scanner(System.in);
 
-		for (int i = 0; i < gList.size(); i++) {
+		while(true) {
 			p = sc.nextLine();
-			
+
 			if (p.equals("q")) { // q입력 --> 종료
 				System.out.println("[입력완료]");
 				break;
 			}
-
+			
+			String[] gArray = p.split(",");
+			String name = gArray[0];
+			price = Integer.parseInt(gArray[1]);
+			count = Integer.parseInt(gArray[2]);
+			
+			Goods goods = new Goods(name, price, count);
+			gList.add(goods);
 		}
+		
+		System.out.println("=============================");
+		
+		for(int i = 0; i<gList.size(); i++) {
+			gList.get(i).showInfo();
+		}
+		
+		for(int i=1; i<gList.size(); i++) {
+			sum += count;
+		}
+		System.out.println("모든 상품의 갯수는 " + sum + "개 입니다.");
+		
+		
+		sc.close();
 	}
 
 }
